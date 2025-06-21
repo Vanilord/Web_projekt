e953d60b8fd02f5047f277af33d943b8cb9b4308
KremenakK <kremenak@voskh.cz>
2025-06-21 19:45:51 +0200

    Add files via upload

--- html/model/test.php
+++ html/model/test.php
@@ -2,7 +2,10 @@
     include "./db_connect.php";
     include "./user_db.php";
 
-    [$jmeno,$heslo,$email,$level]= read_user($conn,"test1");  
-    echo $jmeno." ".$heslo." ".$heslo." ".$level."[/br]";
-
+    [$id,$jmeno,$heslo,$email,$level]= read_user($conn,"test11");  
+    update_user($conn,"test11","heslo","test11@cz","0");
+    [$id,$jmeno,$heslo,$email,$level]= read_user($conn,"test11");  
+    echo "<br>čtení z databáze po změně ".$id." ".$jmeno." hash: ".$heslo." ".$email." ".$level." <br>";
+    echo verifi_user($conn,"test11","heslo1") ? 'Heslo ověřeno<br>' : 'Špatné heslo<br>';
+    
 ?>
--- html/model/user_db.php
+++ html/model/user_db.php
@@ -2,14 +2,12 @@
 
 function read_user($conn,$username){
     $sql="SELECT * FROM `users` WHERE `jmeno` = '$username';";
-    echo $sql;
+    //echo "<br>1".$sql;
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        /*while($row = $result->fetch_assoc()) {
-            print "jmeno: " . $row["jmeno"]. " - heslo: " . $row["heslo"]. " email:" . $row["email"]. " level:". $row["level"]."<br>";
-        };*/
         $row = $result->fetch_assoc();
-        return [$row["jmeno"],$row["heslo"],$row["email"],row["level"]];
+       // print_r ($row);
+        return [$row["id"],$row["jmeno"],$row["heslo"],$row["email"],$row["level"]];
     }   
     else {
         //echo "0 results";
@@ -17,11 +15,11 @@ function read_user($conn,$username){
     };
 }
 
-function if_exist_user($conn,$username){
-    $sql="SELECT * FROM `users` WHERE `jmeno` = '$username';";
-    echo $sql;
+function if_exist_user($conn,$jmeno){
+    $sql="SELECT * FROM `users` WHERE `jmeno` = '$jmeno';";
+    //echo "<br>2".$sql;
     $result = $conn->query($sql);
-    if ($result->num_rows = 0) {
+    if ($result->num_rows == 0) {
         return 0;
     }
     else {
@@ -29,7 +27,37 @@ function if_exist_user($conn,$username){
     }
 };
 
+function update_user($conn,$jmeno,$heslo,$email,$level){
+    [$id,$old_jmeno,$old_heslo,$old_email,$old_level]=read_user($conn,$jmeno);
+    if ($heslo=="0"){ $heslo=$old_heslo;}    
+    if ($email=="0"){ $email=$old_email;}
+    if ($level=="0"){ $level=$old_level;}
+    echo "heslo pro update je ".$heslo;
+    $hash=password_hash($heslo,PASSWORD_DEFAULT);
+    $sql = "UPDATE `users` SET `jmeno` = '$jmeno', `heslo` = '$hash', `email` = '$email', `level` = '$level' WHERE `users`.`id` = '$id';";
+    $result = $conn->query($sql);
+        
+}
 
+function create_user($conn,$jmeno,$heslo,$email,$level){
+    if (!if_exist_user($conn,$jmeno)){
+        $pass_hash=password_hash($heslo,PASSWORD_DEFAULT);
+        $sql = "INSERT INTO `users` (`id`, `jmeno`, `heslo`, `email`, `level`) VALUES (NULL, '$jmeno', '$pass_hash','$email', '$level');";
+        echo "<br>".$sql;
+        $result = $conn->query($sql);
+        return 1;
+    }
+    else {
+        echo "<br>uživatel již existuje";
+        return 0;
+    }
+}
+
+function verifi_user ($conn,$jmeno,$heslo){
+    [$id,$jmeno,$heslo_db,$email,$level]= read_user($conn,$jmeno);  
+     return (password_verify($heslo, $heslo_db));
+        
+}
 
 
 ?>
